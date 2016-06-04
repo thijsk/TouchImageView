dd667910ff69c6d8e60130a3ad283e607157d505
Lorenzo Delgado <lorenzodelgado.dev@gmail.com>
2016-06-04 16:09:17 +0200

    Update project and deploy via jitpack.io

--- example/src/main/java/com/ortiz/touchdemo/ExtendedViewPager.java
+++ example/src/main/java/com/ortiz/touchdemo/ExtendedViewPager.java
@@ -7,7 +7,6 @@ import android.view.View;
 
 import com.ortiz.touchview.TouchImageView;
 
-
 public class ExtendedViewPager extends ViewPager {
 
 	public ExtendedViewPager(Context context) {
@@ -21,11 +20,11 @@ public class ExtendedViewPager extends ViewPager {
 	@Override
 	protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
 	    if (v instanceof TouchImageView) {
-	    	//
-	    	// canScrollHorizontally is not supported for Api < 14. To get around this issue,
-	    	// ViewPager is extended and canScrollHorizontallyFroyo, a wrapper around
-	    	// canScrollHorizontally supporting Api >= 8, is called.
-	    	//
+			/**
+			 * canScrollHorizontally is not supported for Api < 14. To get around this issue,
+			 * ViewPager is extended and canScrollHorizontallyFroyo, a wrapper around
+			 * canScrollHorizontally supporting Api >= 8, is called.
+			 */
 	        return ((TouchImageView) v).canScrollHorizontallyFroyo(-dx);
 	        
 	    } else {
